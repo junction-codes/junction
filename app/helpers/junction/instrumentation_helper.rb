f9f8ffc666d08aa28c3ea942c4bfe3150be9c2d6
James I. Armes <jamesiarmes@gmail.com>
2026-03-14 01:40:40 +0000

    feat: Add OpenTelemetry instrumentation of the plugins system.

--- app/helpers/junction/instrumentation_helper.rb
+++ app/helpers/junction/instrumentation_helper.rb
@@ -3,6 +3,14 @@
 module Junction
   # Helper methods for application-level instrumentation.
   module InstrumentationHelper
+    # Sets an attribute on the current span.
+    #
+    # @param name [String] The name of the attribute.
+    # @param value [Object] The value of the attribute.
+    def span_attribute(name, value)
+      OpenTelemetry::Trace.current_span.set_attribute(name, value)
+    end
+
     # Traces an operation and returns the result.
     #
     # @param name [String] The name of the operation; used as the span name.
