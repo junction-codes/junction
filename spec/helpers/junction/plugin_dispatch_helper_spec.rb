7db9f3c52cbbc21c416a42862ae9845b6491a1ec
James I. Armes <jamesiarmes@gmail.com>
2026-04-16 03:01:55 +0000

    fix: Update plugin spec to use a fake plugin.

--- spec/helpers/junction/plugin_dispatch_helper_spec.rb
+++ spec/helpers/junction/plugin_dispatch_helper_spec.rb
@@ -12,17 +12,28 @@ RSpec.describe Junction::PluginDispatchHelper do
   end
 
   describe "#resolve_plugin_route" do
+    let(:url_helpers) { Junction::Engine.routes.url_helpers }
+    let(:fake_plugin_path) { :junction_plugin_dispatch_spec_fake_path }
+
     before do
       allow(Junction::PluginRegistry).to receive(:plugin_route_helper_entity_classes)
-        .and_return({ component_github_actions_path: Junction::Component })
+        .and_return({ fake_plugin_path => Junction::Component })
+
+      allow(url_helpers).to receive(:respond_to?).and_wrap_original do |method, sym, *rest|
+        sym == fake_plugin_path || method.call(sym, *rest)
+      end
+
+      allow(url_helpers).to receive(fake_plugin_path) do |**kwargs|
+        "/components/#{kwargs.fetch(:namespace)}/#{kwargs.fetch(:name)}/spec-fake-plugin"
+      end
     end
 
     it "expands a sluggable catalog record into namespace/name for plugin routes" do
       component = create(:component, title: "Junction", namespace: "default", name: "junction")
 
-      path = host.send(:resolve_plugin_route, :component_github_actions_path, component)
+      path = host.send(:resolve_plugin_route, fake_plugin_path, component)
 
-      expect(path).to eq("/components/default/junction/component/github/actions")
+      expect(path).to eq("/components/default/junction/spec-fake-plugin")
     end
 
     it "raises when the helper does not exist" do
